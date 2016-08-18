d1700d78904f47a6a69d7d1734eb263b158be398
Jarrett Bariel <jbariel@ecsteam.com>
2016-08-18 12:53:20 -0600

    Rework to have a useful threading model, get producer and consumer working, ensure app still works for status checks later

--- RabbitMQTester/Program.cs
+++ RabbitMQTester/Program.cs
@@ -1,31 +1,136 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
 
 namespace RabbitMQTester
 {
     public class Program
     {
+        static bool isProducer = true;
+        static bool isConsumer = true;
+
+        static Thread ProducerThread;
+        static Thread ConsumerThread;
+        static Thread AppThread;
+        static IWebHost WebHost;
+
         public static void Main(string[] args)
         {
-           var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
-            
-            var host = new WebHostBuilder()
-                .UseConfiguration(config)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+            var config = new ConfigurationBuilder().AddCommandLine(args).AddEnvironmentVariables(prefix: "ASPNETCORE_").Build();
 
-            host.Run();
+            WebHost = new WebHostBuilder().UseConfiguration(config).UseKestrel().UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration().UseStartup<Startup>().Build();
+
+            AppThread = new Thread(() => { WebHost.Run(); });
+            AppThread.IsBackground = true;
+            AppThread.Start();
+
+            if (isProducer)
+            {
+                ProducerThread = new Thread(() => { Send.Start(); });
+                ProducerThread.IsBackground = true;
+                ProducerThread.Start();
+            }
+
+            if (isConsumer)
+            {
+                ConsumerThread = new Thread(() => { Receive.Start(); });
+                ConsumerThread.IsBackground = true;
+                ConsumerThread.Start();
+            }
+
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+
+            // should probably clean up threads here...
+            Send.Stop();
+            Receive.Stop();
+            WebHost.Dispose();
+            AppThread.Join();
+
+            Environment.Exit(0);
+        }
+    }
+
+    class Connection
+    {
+
+        static string HostName = "localhost";
+        static int Port = AmqpTcpEndpoint.UseDefaultPort;
+        static string VirtualHost = "";
+        public static string MyQueue = "";
+        public static string ReceiveUser = "";
+        public static string ReceivePassword = "";
+        public static string SendUser = "";
+        public static string SendPassword = "";
+        
+        public static IModel Connect(string username, string password)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.HostName = HostName;
+            factory.Port = Port;
+            factory.VirtualHost = VirtualHost;
+            factory.UserName = username;
+            factory.Password = password;
+
+            return factory.CreateConnection().CreateModel();
+        }
+    }
+
+    class Send
+    {
+        static Timer SendTimer;
+        static IModel Channel;
+
+        public static void Start()
+        {
+            Console.WriteLine("Starting Producer...");
+            Channel = Connection.Connect(Connection.SendUser, Connection.SendPassword);
+            SendTimer = new Timer((args) =>
+            {
+                Console.WriteLine("Publishing...");
+                Channel.BasicPublish(exchange: "",
+                    routingKey: Connection.MyQueue,
+                    basicProperties: null,
+                    body: Encoding.UTF8.GetBytes(string.Format("Test message: '{0}'", DateTime.Now.ToString("u"))));
+            }, null, 0, 1000);
+
+        }
+
+        public static void Stop()
+        {
+            if (null != SendTimer) SendTimer.Dispose();
+            if (null != Channel) Channel.Dispose();
+        }
+    }
+
+    class Receive
+    {
+        static Timer ReceiveTimer;
+        static IModel Channel;
+
+        public static void Start()
+        {
+            Console.WriteLine("Starting Consumer...");
+            Channel = Connection.Connect(Connection.ReceiveUser, Connection.ReceivePassword);
+            //ReceiveTimer = new Timer((args) => { Console.WriteLine("Consuming..."); }, null, 0, 1000);
+            var consumer = new EventingBasicConsumer(Channel);
+            consumer.Received += (m, ea) =>
+            {
+                Console.WriteLine("   Received message: '{0}'", Encoding.UTF8.GetString(ea.Body));
+            };
+            Channel.BasicConsume(queue: Connection.MyQueue, noAck: true, consumer: consumer);
+        }
+
+        public static void Stop()
+        {
+            if (null != ReceiveTimer) ReceiveTimer.Dispose();
+            if (null != Channel) Channel.Dispose();
         }
     }
 }
--- RabbitMQTester/Startup.cs
+++ RabbitMQTester/Startup.cs
@@ -1,28 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-using System.Text;
+using System;
+using System.Linq;
 using System.Threading;
 
 namespace RabbitMQTester
 {
     public class Startup
     {
-
-        bool isProducer = true;
-        bool isConsumer = true;
-        string receiveUser = "";
-        string receivePassword = "";
-        string sendUser = "";
-        string sendPassword = "";
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -40,85 +28,10 @@ namespace RabbitMQTester
                 app.UseDeveloperExceptionPage();
             }
 
-            RunRabbit();
-
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Welcome to RabbitMQTester!");
             });
         }
-
-        void RunRabbit(){
-            if (isProducer) {
-                var producerThread = new Thread(
-                    () =>
-                    {
-                        Send.Start(Connection.Connect(sendUser,sendPassword));
-                    });
-                producerThread.IsBackground = true;
-                producerThread.Start();
-            }
-
-            if (isConsumer) {
-                var consumerThread = new Thread(
-                    () =>
-                    {
-                        Receive.Start(Connection.Connect(receiveUser,receivePassword));
-                    });
-                consumerThread.IsBackground = true;
-                consumerThread.Start();
-                
-            }
-
-        }
     }
-
-    class Connection {
-        
-        public static string MyQueue = "hello-world";
-
-        public static IModel Connect(string username, string password){
-            var factory = new ConnectionFactory();
-            
-            factory.HostName = "localhost";
-            factory.Port = AmqpTcpEndpoint.UseDefaultPort;
-            factory.VirtualHost = "";
-            factory.UserName = username;
-            factory.Password = password;
-
-            return factory.CreateConnection().CreateModel();
-        }
-    }
-
-    class Send {
-        public static void Start(IModel model){
-            while(true){
-                model.BasicPublish(exchange: "",
-                    routingKey: Connection.MyQueue,
-                    basicProperties: null,
-                    body: Encoding.UTF8.GetBytes(string.Format("Test message: '{0}'",DateTime.Now.ToString("u"))));
-
-                Thread.Sleep(1000);
-            }
-        }
-    }
-
-    class Receive {
-        public static void Start(IModel model){
-            var consumer = new EventingBasicConsumer(model);
-            consumer.Received += (m, ea) =>
-            {
-                Task.Run(() => {
-                    var body = ea.Body;
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine("   Received message: '{0}'", message);
-                    model.BasicAck(ea.DeliveryTag, false);
-                });
-            };
-            model.BasicConsume(queue: Connection.MyQueue,
-                                 noAck: true,
-                                 consumer: consumer);
-        }
-    }
-
 }
